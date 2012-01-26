6592ebae8fd6eecbc2397a1ffa74fec284320ca7
Adam Fisk <a@littleshoot.org>
2012-01-25 18:44:39 -0800

    reorganizing

--- services/src/main/java/org/lastbamboo/client/services/SystemTray.java
+++
@@ -1,9 +0,0 @@
-package org.lastbamboo.client.services;
-
-/**
- * Interface for system tray implementations.
- */
-public interface SystemTray
-    {
-
-    }
--- site/uploadExe.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-java -jar ../../../../../../usr/local/aws-0.3/s3.jar -putp littleshoot $1 
